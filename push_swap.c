a3f69e0d068e32f31b0884835d06330e0c908598
jcampagn <vesslan@hotmail.fr>
2022-01-02 17:01:28 +0100

    okkkkk

--- push_swap.c
+++ push_swap.c
@@ -6,55 +6,46 @@
 /*   By: vess <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/25 17:02:17 by vess              #+#    #+#             */
-/*   Updated: 2022/01/01 18:24:03 by vess             ###   ########.fr       */
+/*   Updated: 2022/01/02 17:01:10 by vess             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main (int ac, char **av)
+t_list	*create_list(int ac, char **av, t_list **stack_a)
 {
+	int	i;
 
-    t_list *stack_a = NULL;
-	t_list *stack_b = NULL;
-    ft_lstadd_front(&stack_a, ft_lstnew("3"));
-     ft_lstadd_front(&stack_a, ft_lstnew("2"));
-     ft_lstadd_front(&stack_a, ft_lstnew("1"));
-	 ft_lstadd_front(&stack_b, ft_lstnew("3"));
-     ft_lstadd_front(&stack_b, ft_lstnew("2"));
-     ft_lstadd_front(&stack_b, ft_lstnew("1"));
-	 int i = 0;
-//	 ft_sa(&stack_a, 1);
-//	 ft_sb(&stack_b, 1);
-//	ft_ss(&stack_a, &stack_b);
-//	ft_ra(&stack_a, 1);
-//	ft_rb(&stack_b, 1);
-//	ft_rr(&stack_a, &stack_b);
-//	ft_rra(&stack_a, 1);
-//	ft_rrb(&stack_b, 1);
-//	ft_rrr(&stack_a, &stack_b);
-//ft_pb(&stack_a, &stack_b);
-	 while (i < 2)
-	 {
-		i++;
-		printf("%s\n", (char *)stack_a->content);
-		stack_a = stack_a->next;
-	 }
-	 printf("-------------------------\n"); 
-	 printf("A\n");
-	i = 0;
-	while (i < 4)
-	 {
-		i++;
-		printf("%s\n", (char *)stack_b->content);
-		stack_b = stack_b->next;
-	 }
-	 printf("-------------------------\n"); 
-	 printf("B\n");
-
-
-
-	return (0);
+	i = ac - 1;
+	while (i > 0)
+	{
+		ft_lstadd_front(&*stack_a, ft_lstnew(av[i]));
+		i--;
+	}
+	return (*stack_a);
 }
 
+void	print_list(t_list *stack_a)
+{
+	while (stack_a != NULL)
+	{
+		printf("%s\n", (char *)stack_a->content);
+		stack_a = stack_a->next;
+	}
+	printf("-------------------------\n");
+	printf("A\n");
+}
 
+int	main(int ac, char **av)
+{
+	t_list	*stack_a;
+	t_list	*stack_b;
+	int		i;
+
+	stack_a = NULL;
+	stack_b = NULL;
+	stack_a = create_list(ac, av, &stack_a);
+	i = 0;
+	print_list(stack_a);
+	return (0);
+}
